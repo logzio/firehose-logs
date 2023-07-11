f6abc87cbbb1a275fa501fafd5a955df44aad07b
mirii1994 <miri.ignatiev@logz.io>
2023-07-11 19:04:06 +0300

    init

---
+++ go.mod
@@ -0,0 +1,15 @@
+module main
+
+go 1.19
+
+require (
+	github.com/aws/aws-lambda-go v1.41.0
+	github.com/aws/aws-sdk-go v1.44.294
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
---
+++ utils.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+const (
+	envServices                  = "SERVICES"
+	envAwsRegion                 = "AWS_REGION"               // reserved env
+	envFunctionName              = "AWS_LAMBDA_FUNCTION_NAME" // reserved env
+	envFirehoseArn               = "FIREHOSE_ARN"
+	envAccountId                 = "ACCOUNT_ID"
+	envCustomGroups              = "CUSTOM_GROUPS"
+	envAwsPartition              = "AWS_PARTITION"
+	envPutSubscriptionFilterRole = "PUT_SF_ROLE"
+
+	valuesSeparator        = ","
+	emptyString            = ""
+	lambdaPrefix           = "/aws/lambda/"
+	subscriptionFilterName = "logzio_firehose"
+)
+
+func getServices() []string {
+	servicesStr := os.Getenv(envServices)
+	if servicesStr == emptyString {
+		return nil
+	}
+
+	servicesStr = strings.ReplaceAll(servicesStr, " ", "")
+	return strings.Split(servicesStr, valuesSeparator)
+}
+
+func getServicesMap() map[string]string {
+	return map[string]string{
+		"apigateway":       "/aws/apigateway/",
+		"rds":              "/aws/rds/cluster/",
+		"cloudhsm":         "/aws/cloudhsm/",
+		"cloudtrail":       "aws-cloudtrail-logs-",
+		"codebuild":        "/aws/codebuild/",
+		"connect":          "/aws/connect/",
+		"elasticbeanstalk": "/aws/elasticbeanstalk/",
+		"ecs":              "/aws/ecs/",
+		"eks":              "/aws/eks/",
+		"aws-glue":         "/aws/aws-glue/",
+		"aws-iot":          "AWSIotLogsV2",
+		"lambda":           "/aws/lambda/",
+		"macie":            "/aws/macie/",
+		"amazon-mq":        "/aws/amazonmq/broker/",
+	}
+}
+
+func getCustomPaths() []string {
+	pathsStr := os.Getenv(envCustomGroups)
+	if pathsStr == emptyString {
+		return nil
+	}
+
+	pathsStr = strings.ReplaceAll(pathsStr, " ", "")
+	return strings.Split(pathsStr, valuesSeparator)
+}
+
+func listContains(s string, l []string) bool {
+	for _, item := range l {
+		if s == item {
+			return true
+		}
+	}
+
+	return false
+}
